4877cb156dcc56b221093e533098e2e367890e69
Alex Kim <312akim@gmail.com>
2021-08-15 14:42:52 -0700

    Fix: Fixed AppLoading import

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
-import { AppLoading } from 'expo-app-loading';
+import AppLoading from 'expo-app-loading';
 
 const fetchFonts = () => {
   Font.loadAsync({
