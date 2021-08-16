9ee00f368bb944141653e69a4d9719e38e504a36
Alex Kim <312akim@gmail.com>
2021-08-16 02:35:54 -0700

    Feat: CategoriesScreen initial grid and header styling

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Platform } from 'react-native';
+import Colors from '../themes/Colors';
 
 import { CATEGORIES } from '../data/dummy-data';
 
-const renderGridItem = (itemData) => {
-    return (
-        <View style={styles.gridItem}>
-            <Text>
-                {itemData.item.title}
-            </Text>
-        </View>
-    )
-}
 
 const CategoriesScreen = props => {
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity 
+                style={styles.gridItem} 
+                onPress={() => {
+                    props.navigation.navigate({routeName: 'CategoryMeals'});
+            }}>
+                <View>
+                    <Text>
+                        {itemData.item.title}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
     return (
         <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
     )
 };
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+}
 
 const styles = StyleSheet.create({
     screen: {
