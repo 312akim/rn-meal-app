7a7dc53b01ae10bf8eb6851aaec66e123f504875
Alex Kim <312akim@gmail.com>
2021-08-19 22:45:52 -0700

    Update: Platform specific tab navigator and styling

--- navigation/MealsNavigator.tsx
+++ navigation/MealsNavigator.tsx
@@ -3,6 +3,7 @@ import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
@@ -30,18 +31,32 @@ const MealsNavigator = createStackNavigator({
     }
 });
 
-const MealsFavTabNavigator = createBottomTabNavigator({
+const tabScreenConfig = {
     Meals: {screen: MealsNavigator, navigationOptions: {
         tabBarIcon: (tabInfo) => {
             return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
+        },
+        // Only supported on Shifting true
+        tabBarColor: Colors.primaryColor,
+        /* On shiftin false, sets config for bar
+        barStyle: {
+            backgroundColor: Colors.primaryColor
         }
+        */
     }},
     Favorites: {screen: FavoritesScreen, navigationOptions: {
         tabBarIcon: (tabInfo) => {
             return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
-        }
+        },
+        tabBarColor: Colors.accentColor
     }}
-}, {
+}
+
+const MealsFavTabNavigator = Platform.OS === 'android' ? createMaterialBottomTabNavigator(tabScreenConfig, {
+    activeColor: 'white',
+    shifting: true,
+}) : createBottomTabNavigator(tabScreenConfig,
+{
     tabBarOptions: {
         activeTintColor: Colors.accentColor,
     }
